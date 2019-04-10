8ee3db1b34cba72eafdbec713622cdf55d7182e6
RoccoDev <roccodev.business@gmail.com>
2019-04-10 14:35:20 +0200

    Added more UI stuff

---
+++ README.md
@@ -0,0 +1,4 @@
+# Commenter
+Converts **paragraphs** of all sizes to **comment blocks** of a variety of languages.
+
+The website is available [here](https://rocco.dev/commenter).
--- js/index.js
+++ js/index.js
@@ -15,7 +15,50 @@
 // You should have received a copy of the GNU General Public License
 // along with commenter.  If not, see <http://www.gnu.org/licenses/>.
 
-Commenter = {}
+Commenter = {
+    convert: () => {
+        let userInputElement = document.getElementById("userIn")
+        let outputElement = document.getElementById("userOut")
+        let selector = document.getElementById("langSelector")
+        let multiCheck = document.getElementById("multiCheckbox")
+
+        let userInput = userInputElement.value
+
+        if(!userInput || !selector.value) return
+
+        let selectedLanguage = Commenter.languages[parseInt(selector.value)]
+        let preferBlock = multiCheck.checked
+
+        // Force block if the language doesn't support single
+        // and vice versa
+        if(!selectedLanguage.singleComment)
+            preferBlock = true
+        else if(!selectedLanguage.blockStart)
+            preferBlock = false
+
+        // Clear output
+        outputElement.innerHTML = ""
+
+        // What should be added before each line
+        let decoration = preferBlock ? selectedLanguage.blockLinePrefix : selectedLanguage.singleComment
+
+        if(preferBlock)
+            outputElement.innerHTML += `${selectedLanguage.blockStart}\n`
+
+        userInput.split("\n").forEach(line => {
+            outputElement.innerHTML += `${decoration} ${line}\n`
+        })
+
+        // Remove extra \n
+        outputElement.innerHTML = outputElement.innerHTML.substr(0, outputElement.innerHTML.length - 1)
+
+        if(preferBlock)
+            outputElement.innerHTML += `\n${selectedLanguage.blockEnd}`
+
+        // Resize the textarea (Materialize won't do it automatically)
+        M.textareaAutoResize(outputElement)
+    }
+}
 
 // Load the language data
 loadLanguages().then(result => {
